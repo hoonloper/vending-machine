82da4218917204f30dcacb286a0be905a5942e36
hoonloper <yonghoonj98@gmail.com>
2023-11-10 14:32:34 +0900

    Feat: 데이터 검증 유틸들 구현

--- utils.js
+++ utils.js
@@ -2,4 +2,30 @@ const log = (message) => {
   process.stdout.write(message);
 };
 
-export { log };
+const validString = (value) => typeof value === "string";
+
+const validNumber = (value) => typeof value === "number";
+
+const validFilledString = (value) =>
+  validString(value) && value.trim().length > 0;
+
+const validStrictNumber = (value) => validNumber(value) && !Number.isNaN(value);
+
+const validNumberString = (value) => validString(value) && /^\d+$/.test(value);
+
+const validStringRange = (value, minLength = 1, maxLength = 20) =>
+  validString(value) &&
+  !value.startsWith(" ") &&
+  !value.endsWith(" ") &&
+  value.length >= minLength &&
+  value.length <= maxLength;
+
+export {
+  log,
+  validString,
+  validNumber,
+  validFilledString,
+  validStrictNumber,
+  validNumberString,
+  validStringRange,
+};
