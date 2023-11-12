5828c936ba674ea7d75d687644fe7f62f862b062
hoonloper <yonghoonj98@gmail.com>
2023-11-12 14:31:05 +0900

    Feat: 각 상황별 에러 검증 정적 메서드 추가

--- common/CustomError.js
+++ common/CustomError.js
@@ -2,15 +2,24 @@ class InvalidError extends Error {
   constructor(message) {
     super(`INVALID:${message}`);
   }
+  static isInvalidError(error) {
+    return error instanceof InvalidError;
+  }
 }
 class NotFoundError extends Error {
   constructor(message) {
     super(`NOT_FOUND:${message}`);
   }
+  static isNotFoundError(error) {
+    return error instanceof NotFoundError;
+  }
 }
 class ServerError extends Error {
   constructor(message) {
     super(`SERVER:${message}`);
+  }
+  static isServerError(error) {
+    return error instanceof ServerError;
   }
 }
 
