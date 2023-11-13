ef0f79099a6c745a25af08a3f4d4d8f4997eef65
hoonloper <yonghoonj98@gmail.com>
2023-11-13 22:25:56 +0900

    Test: DrinkManager 실패 테스트 추가

--- specs/models/DrinkManager.js
+++ specs/models/DrinkManager.js
@@ -60,37 +60,20 @@ describe("음료 매니저 모델 테스트", () => {
       assert.strictEqual(true, copyDrinkManager !== drinkManager);
     });
   });
-  //   describe("실패", () => {
-  //     let drinkManager = null;
-  //     beforeEach(() => {
-  //       drinkManager = new DrinkManager(NAME, PRICE, COUNT);
-  //     });
+  describe("실패", () => {
+    let drinkManager = null;
+    beforeEach(() => {
+      drinkManager = new DrinkManager();
+    });
 
-  //     it("잘못된 생성자 주입", () => {
-  //       assert.throws(() => new Drink(123, PRICE, COUNT), InvalidError);
-  //       assert.throws(() => new Drink(true, PRICE, COUNT), InvalidError);
-  //       assert.throws(() => new Drink(null, PRICE, COUNT), InvalidError);
-  //       assert.throws(() => new Drink(NAME, -1, COUNT), InvalidError);
-  //       assert.throws(() => new Drink(NAME, "1234", COUNT), InvalidError);
-  //       assert.throws(() => new Drink(NAME, true, COUNT), InvalidError);
-  //       assert.throws(() => new Drink(NAME, null, COUNT), InvalidError);
-  //       assert.throws(() => new Drink(NAME, PRICE, -1), InvalidError);
-  //       assert.throws(() => new Drink(NAME, PRICE, "1234"), InvalidError);
-  //       assert.throws(() => new Drink(NAME, PRICE, true), InvalidError);
-  //       assert.throws(() => new Drink(NAME, PRICE, null), InvalidError);
-  //     });
-
-  //     it("음료 품절일 때 구매", () => {
-  //       drink.sold();
-  //       drink.sold();
-  //       drink.sold();
-  //       drink.sold();
-  //       drink.sold();
-  //       assert.strictEqual(null, drink.sold());
-  //     });
-
-  //     it("음료 인스턴스 검증", () => {
-  //       assert.strictEqual(false, Drink.isDrink(new Error()));
-  //     });
-  //   });
+    it("잘못된 생성자 주입", () => {
+      assert.throws(() => new DrinkManager([123]), InvalidError);
+      assert.throws(() => new DrinkManager(["123"]), InvalidError);
+      assert.throws(() => new DrinkManager(-1), InvalidError);
+      assert.throws(() => new DrinkManager(123), InvalidError);
+      assert.throws(() => new DrinkManager("123"), InvalidError);
+      assert.throws(() => new DrinkManager(true), InvalidError);
+      assert.throws(() => new DrinkManager(null), InvalidError);
+    });
+  });
 });
