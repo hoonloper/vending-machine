ac3f324a156c2a23e1dc4ccdd87490b0fae1324d
hoonloper <yonghoonj98@gmail.com>
2023-11-13 20:24:21 +0900

    Feat: CardStage 로깅 개선

--- stages/CardStage.js
+++ stages/CardStage.js
@@ -1,29 +1,35 @@
-const { InvalidError, ServerError } = require("../common/CustomError");
+const { InvalidError } = require("../common/CustomError");
 const { COMMAND } = require("../common/constant");
-const { log, logDivider, logs } = require("../common/utils");
+const {
+  log,
+  getLoggingDivider,
+  addLineBreakOfTexts,
+} = require("../common/utils");
 const Card = require("../models/Card");
 
 class CardStage {
   card = null;
 
   run() {
-    logDivider();
-    if (Card.isCard(this.#getCard())) {
-      log(
-        `💵💵💵💵💵 [현재까지 사용한 금액: ${this.#getCard().getPrice()}원] 💵💵💵💵💵\n`
-      );
-      this.#done();
-    } else {
-      this.logMessage();
-    }
-    logDivider();
+    const card = this.#getCard();
+
+    const divider = getLoggingDivider();
+    const message = Card.isCard(card)
+      ? addLineBreakOfTexts(
+          `💵💵💵💵💵 [현재까지 사용한 금액: ${card.getPrice()}원] 💵💵💵💵💵`,
+          this.#done()
+        )
+      : this.getMessage();
+    log(addLineBreakOfTexts(divider, message, divider));
+
+    return null;
   }
 
-  logMessage() {
-    log(
-      "카드번호(16자리), 만료(월/년), 생년월일(6자리)을 ':'로 구분지어 작성해 주세요."
+  getMessage() {
+    return addLineBreakOfTexts(
+      "카드번호(16자리), 만료(월/년), 생년월일(6자리)을 ':'로 구분지어 작성해 주세요.",
+      "EX: 1234567818273645:12/28:900101"
     );
-    log("EX: 1234567818273645:12/28:900101");
   }
 
   do(command) {
@@ -35,26 +41,29 @@ class CardStage {
   }
 
   #progress() {
-    if (Card.isCard(this.#getCard())) {
-      logDivider();
-      logs(
-        "카드 등록이 정상적으로 완료되었습니다.",
-        "결제를 진행하시려면 '결제'를 입력해 주세요."
-      );
-      logDivider();
-      return this.#getCard();
-    }
-    logDivider();
-    log("🚨 카드 정보가 존재하지 않습니다. 🚨");
-    this.logMessage();
-    logDivider();
+    const card = this.#getCard();
+
+    const divider = getLoggingDivider();
+    const message = Card.isCard(card)
+      ? addLineBreakOfTexts(
+          "카드 등록이 정상적으로 완료되었습니다.",
+          "결제를 진행하시려면 '결제'를 입력해 주세요."
+        )
+      : addLineBreakOfTexts(
+          "🚨 카드 정보가 존재하지 않습니다. 🚨",
+          this.getMessage()
+        );
+    log(addLineBreakOfTexts(divider, message, divider));
+
+    return Card.isCard(card) ? card : null;
   }
 
   #retry() {
     this.setCard(null);
-    logDivider();
-    this.logMessage();
-    logDivider();
+
+    const divider = getLoggingDivider();
+    log(addLineBreakOfTexts(divider, this.getMessage(), divider));
+
     return null;
   }
 
@@ -67,19 +76,21 @@ class CardStage {
     const [number, expiredDate, birthDay] = command.split(":");
     this.setCard(new Card(number, expiredDate, birthDay));
 
-    logDivider();
-    this.#done();
-    logDivider();
+    const divider = getLoggingDivider();
+    const message = this.#done();
+    log(addLineBreakOfTexts(divider, message, divider));
+
     return null;
   }
 
   #done() {
     const card = this.#getCard();
-    log(`고객님의 카드 정보는 다음과 같습니다.\n`);
-    log(`- 카드번호: ${card.getNumber()}`);
-    log(`- 만료일: ${card.getExpiredDate()}`);
-    log(`- 생년월일: ${card.getBirthDay()}`);
-    log(
+
+    return addLineBreakOfTexts(
+      `고객님의 카드 정보는 다음과 같습니다.\n`,
+      `- 카드번호: ${card.getNumber()}`,
+      `- 만료일: ${card.getExpiredDate()}`,
+      `- 생년월일: ${card.getBirthDay()}`,
       `\n카드 결제 희망 - '${COMMAND.IN_PROGRESS}'\n정보 재입력 희망 - '${COMMAND.RETRY}'\n종료 - '${COMMAND.END}'`
     );
   }
@@ -87,6 +98,7 @@ class CardStage {
   copy() {
     const newCardStage = new CardStage();
     newCardStage.setCard(this.#getCard().copy());
+
     return newCardStage;
   }
 
