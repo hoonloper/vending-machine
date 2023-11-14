ca07dbd5fa91ffa4d1b6a9e5575384f6e184e307
hoonloper <yonghoonj98@gmail.com>
2023-11-14 21:45:26 +0900

    Test: runner에 stage 테스트 등록

--- specs/runner.spec.js
+++ specs/runner.spec.js
@@ -4,9 +4,19 @@ const path = require("node:path");
 
 const ROOT_PATH = "./specs";
 const MODULES = ["Launcher", "LauncherLogger"];
+const STAGES = [
+  "CardStage",
+  "CashStage",
+  "DrinkStage",
+  "PaymentStage",
+  "StageManager",
+].slice(0, 1); // 각 스테이지 테스트 코드 작성시 하나씩 풀기
 const MODELS = ["Card", "Cash", "Drink", "DrinkManager"];
 const COMMON = ["CustomError", "utils"];
 
+const pathStages = STAGES.map((stage) =>
+  path.resolve(`${ROOT_PATH}/stages/${stage}.spec`)
+);
 const pathModules = MODULES.map((module) =>
   path.resolve(`${ROOT_PATH}/${module}.spec`)
 );
@@ -17,6 +27,6 @@ const pathCommon = COMMON.map((common) =>
   path.resolve(`${ROOT_PATH}/common/${common}.spec`)
 );
 
-run({ files: [...pathModules, ...pathModels, ...pathCommon] })
+run({ files: [...pathStages, ...pathModules, ...pathModels, ...pathCommon] })
   .compose(tap)
   .pipe(process.stdout);
