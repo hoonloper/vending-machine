177c9a1f8b5359308dcf38d3e0f56551f5b62744
hoonloper <yonghoonj98@gmail.com>
2023-11-14 19:02:31 +0900

    Test: 전체 테스트 실행에 common 디렉토리 추가

--- specs/runner.spec.js
+++ specs/runner.spec.js
@@ -4,9 +4,15 @@ const path = require("node:path");
 
 const ROOT_PATH = "./specs";
 const MODELS = ["Card", "Cash", "Drink", "DrinkManager"];
+const COMMON = ["CustomError", "utils"];
 
 const pathModels = MODELS.map((model) =>
   path.resolve(`${ROOT_PATH}/models/${model}.spec`)
 );
+const pathCommon = COMMON.map((common) =>
+  path.resolve(`${ROOT_PATH}/common/${common}.spec`)
+);
 
-run({ files: pathModels }).compose(tap).pipe(process.stdout);
+run({ files: [...pathModels, ...pathCommon] })
+  .compose(tap)
+  .pipe(process.stdout);
