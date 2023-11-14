7e16311133218ea30de3ccd962e2605ae0759ca4
hoonloper <yonghoonj98@gmail.com>
2023-11-14 17:20:05 +0900

    Test: 전체 테스트 코드 러너 추가

---
+++ specs/runner.spec.js
@@ -0,0 +1,12 @@
+const { tap } = require("node:test/reporters");
+const { run } = require("node:test");
+const path = require("node:path");
+
+const ROOT_PATH = "./specs";
+const MODELS = ["Card", "Cash", "Drink", "DrinkManager"];
+
+const pathModels = MODELS.map((model) =>
+  path.resolve(`${ROOT_PATH}/models/${model}`)
+);
+
+run({ files: pathModels }).compose(tap).pipe(process.stdout);
