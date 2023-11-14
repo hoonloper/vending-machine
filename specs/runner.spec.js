4944b657a3a950b4c376b7e91a4f08fe44d1048c
hoonloper <yonghoonj98@gmail.com>
2023-11-14 17:21:42 +0900

    Test: 테스트 파일 네이밍 컨벤션 .spec 적용

--- specs/runner.spec.js
+++ specs/runner.spec.js
@@ -6,7 +6,7 @@ const ROOT_PATH = "./specs";
 const MODELS = ["Card", "Cash", "Drink", "DrinkManager"];
 
 const pathModels = MODELS.map((model) =>
-  path.resolve(`${ROOT_PATH}/models/${model}`)
+  path.resolve(`${ROOT_PATH}/models/${model}.spec`)
 );
 
 run({ files: pathModels }).compose(tap).pipe(process.stdout);
