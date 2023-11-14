8ac9d89a08a7907c98e59bd2a963b38771be2106
hoonloper <yonghoonj98@gmail.com>
2023-11-14 22:24:05 +0900

    Test: DrinkStage 실패 테스트 작성

--- specs/stages/DrinkStage.spec.js
+++ specs/stages/DrinkStage.spec.js
@@ -1,6 +1,5 @@
 const { describe, beforeEach, it } = require("node:test");
 const assert = require("assert");
-const { COMMAND } = require("../../app/common/constant");
 const DrinkStage = require("../../app/stages/DrinkStage");
 const Drink = require("../../app/models/Drink");
 
@@ -17,6 +16,10 @@ describe("음료 단계 테스트", () => {
       assert.strictEqual(null, drinkStage.run());
       assert.strictEqual(null, drinkStage.logMessage());
     });
+    it("새로운 음료 선택", () => {
+      drinkStage.do("물");
+      drinkStage.do("커피");
+    });
     it("복사 검증", () => {
       // 음료를 등록
       assert.ok(Drink.isDrink(drinkStage.do("콜라")));
@@ -26,6 +29,15 @@ describe("음료 단계 테스트", () => {
       assert.ok(copyDrinkStage instanceof DrinkStage);
       assert.notStrictEqual(copyDrinkStage, drinkStage);
       assert.strictEqual(true, copyDrinkStage !== drinkStage);
+    });
+  });
+  describe("실패", () => {
+    let drinkStage;
+    beforeEach(() => {
+      drinkStage = new DrinkStage();
+    });
+    it("없는 음료 입력", () => {
+      assert.ok(!Drink.isDrink(drinkStage.do("사이다")));
     });
   });
 });
